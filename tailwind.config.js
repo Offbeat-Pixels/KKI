6be264c82ce7c3466c1e481ab9589eb8c3f9366e
Nitin-Rawat <docock0@gmail.com>
2024-12-24 13:07:30 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"], // Ensure Tailwind scans your HTML files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
